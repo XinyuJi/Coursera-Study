c2f8d8cfd159483309fdd2f05b2eeb988bbeaff4
XinyuJi <56640682+XinyuJi@users.noreply.github.com>
2020-07-27 14:51:05 +0200

    Create extracting_data_from_xml.py

---
+++ Using Python to Access Web Data/week5_web_services_and_xml/extracting_data_from_xml.py
@@ -0,0 +1,14 @@
+import urllib.request, urllib.error
+import xml.etree.ElementTree as ET
+
+#url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
+url = 'http://py4e-data.dr-chuck.net/comments_803338.xml'
+output = urllib.request.urlopen(url).read()
+tree = ET.fromstring(output)
+
+total = 0
+for comments in tree.findall('comments'):
+    for comment in comments.findall('comment'):
+        total += int(comment.find('count').text)
+
+print(total)
